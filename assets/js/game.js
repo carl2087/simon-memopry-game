892668eb21a590201e6ce8019bf2a3ef91a515a9
Carl <carlelli5@hotmail.co.uk>
2023-01-01 15:35:52 +0000

    added showturns function with tests

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
+    turnNumber: 0,
 }
 
 function newGame() {
@@ -16,7 +17,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
 function showScore() {
@@ -30,5 +31,16 @@ function lightsOn(circ) {
     }, 400);
 }
 
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        };
+    }, 800);
+};
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
