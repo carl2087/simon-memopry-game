ba0db2ea4f0a0e4d2eeebc7f7d94ff5301da3982
Carl <carlelli5@hotmail.co.uk>
2023-01-02 21:10:12 +0000

    added rest of game code

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,8 @@ let game = {
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
     turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false,
 }
 
 function newGame() {
@@ -11,15 +13,18 @@ function newGame() {
     game.playerMoves = [];
     game.currentGame = [];
     game.turnNumber = 0;
-    for (let circle of document.getElementsByClassName("circle")) {
-        if (circle.getAttribute("data-listener") !== "true") {
-            circle.addEventListener("click", (e) => {
-                let move = e.target.getAttribute("id");
-                lightsOn(move);
-                game.playerMoves.push(move);
-                playerTurn();
+    for (let circle of document.getElementsByClassName("circle")) {         // steps through elements with class circle
+        if (circle.getAttribute("data-listener") !== "true") {              // checks if not equal to true
+            circle.addEventListener("click", (e) => {                       // adds event listener click and pass in e as event object
+                if (game.currentGame.length > 0 && !game.turnInProgress) {  // if statement to only allow clicks if the currentGame array is greater than zero and if game is in progress
+                let move = e.target.getAttribute("id");                     // attaches e to variable move by getting id attribute
+                game.lastButton = move;                                     // stores move in lastButton 
+                lightsOn(move);                                             // calls lights on function and uses variable move to light up correct element
+                game.playerMoves.push(move);                                // pushes move into game.playerMoves
+                playerTurn();                                               // calls player turn function
+                }                                                   
             });
-            circle.setAttribute("data-listener", "true");
+            circle.setAttribute("data-listener", "true");                   // sets data listener attribute to true which makes the test pass
         }
     }
     showScore();
@@ -44,15 +49,31 @@ function lightsOn(circ) {
 }
 
 function showTurns() {
+    game.turnInProgress = true;
     game.turnNumber = 0;
     let turns = setInterval(() => {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         };
     }, 800);
 };
 
+function playerTurn () {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
--- assets/js/tests/game.test.js
+++ assets/js/tests/game.test.js
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game")
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game")
+
+jest.spyOn(window, "alert").mockImplementation(() => { });
 
 beforeAll(() => {
     let fs = require("fs");
@@ -30,6 +32,15 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress value is false", () => {
+        expect("turnInProgress" in game).toBe(true);
     });
 });
 
@@ -91,5 +102,25 @@ describe("gameplay works correctly", () => {
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
+    });
+    test("should increment the score if the if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
+    });
+    test("checks if turnInProgress is equal to true", () => {
+        showTurns();
+        expect(game.turnInProgress).toEqual(true);
+    });
+    test("clicking during th eocmputers sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
     });
 });
